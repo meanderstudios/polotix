58710fd0059a5ff85bd5976bc8f97d11133f2247
Geekcoder127 <awesome2723@oultook.com>
2017-07-25 22:17:50 +0100

    Extreme Nav Bar Changes

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<meta charset='utf-8'>
+	<title>Polotix | Home</title>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<link rel='stylesheet' type='text/css' href='resources/css/main.css'>
+	<link rel='icon' type='img/icon' href='resources/img/favicon.ico'>
+	<link href="https://fonts.googleapis.com/css?family=Droid+Sans|Raleway" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700" rel="stylesheet">
+</head>
+
+<body>
+
+<?php include "resources/html/nav.html"; ?>
+
+
+</body>
+
+</html>
